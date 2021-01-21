ee41bff0ac54b276ce782b237be3273e3c7a5736
Krishna Karthik N <71656457+KrishnaKarthikN@users.noreply.github.com>
2021-01-21 12:23:32 +0530

    Commit 1

--- src/com/xworkz/collegeTester/Tester.java
+++ src/com/xworkz/collegeTester/Tester.java
@@ -1,5 +1,8 @@
 package com.xworkz.collegeTester;
 
+import java.util.Scanner;
+
+import com.xworkz.college.dao.CollegeDAO;
 import com.xworkz.college.dao.CollegeDAOImpl;
 import com.xworkz.college.dto.CollegeDTO;
 
@@ -7,11 +10,31 @@ public class Tester {
 
 	public static void main(String[] args) {
 		
-		CollegeDTO collegeDTO = new CollegeDTO(03, "BMSCE", "Bangalore", 1600, 300, 6);
-		CollegeDAOImpl daoImpl = new CollegeDAOImpl();
-		//daoImpl.saveCollege(collegeDTO);
-		//daoImpl.fetchCollege(1);
-		daoImpl.updateCollege(02);
+		System.out.println("1 ---> Save");
+		System.out.println("2 ---> Fetch");
+		System.out.println("3 ---> Update");
+		System.out.println("4 ---> Delete");
+		
+		CollegeDTO collegeDTO = new CollegeDTO();
+		CollegeDAO daoImpl = new CollegeDAOImpl();
+		
+		Scanner scanner = new Scanner(System.in);
+		int choice = scanner.nextInt();
+		switch(choice) {
+		
+		case 1 : daoImpl.saveCollege(collegeDTO);
+			break;
+		case 2 : daoImpl.fetchCollege(2);	
+			break;
+		case 3 : daoImpl.updateCollege(3);
+			break;
+		case 4 : daoImpl.removeCollege(4);	
+			break;
+		default : System.out.println();
+		}
+		
+ 	scanner.close();
+		
 	}
 	
 
